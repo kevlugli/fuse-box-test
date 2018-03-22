a4ec4d306be9a2b0c1e3906c0971ebae13df4d6d
Kev Lugli <kevlugli@gmail.com>
2018-03-22 19:09:21 -0300

    Correction on react module

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Button} from 'semantic-ui-react
+import {Button} from 'semantic-ui-react'
 
-export default class ApplicationComponent extends React.Component<{},{}> {
-    render() {
-        return <div>
+const element = <div>
+            Testing with button...
             <Button></Button>
         </div>
-    }
-}
 
-ReactDOM.render(ApplicationComponent, document.getElementById('app'))
+ReactDOM.render(element, document.getElementById('app'))
+
